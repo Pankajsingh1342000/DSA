adf02074541db26c7b4580c1462418df1b23a8a2
Pankaj Singh <pankajsingh13042000@gmail.com>
2022-06-14 13:39:58 +0530

    Implemented Binary Search Algorithm

--- Algorithms/binarySearch.cpp
+++ Algorithms/binarySearch.cpp
@@ -1,2 +1,29 @@
 #include<iostream>
 using namespace std;
+
+int binarySearch(int arr[], int n, int key){
+    int left=0, right = n-1;
+    int mid = left + ((right - left)/2);
+    while(left<=right){
+        if(key == arr[mid]){
+            return mid;
+        }
+        else if(key < arr[mid]){
+            right = mid-1;
+        }
+        else{
+            left = mid+1;
+        }
+
+        mid = left + ((right - left)/2);
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={3,5,7,8,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int key;
+    cin>>key;
+    cout<<binarySearch(arr,n,key);
+}
