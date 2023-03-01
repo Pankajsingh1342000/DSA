41c019073b685fb8375ff76832349763a05def33
Pankaj Singh <pankajsingh13042000@gmail.com>
2023-03-01 18:47:36 +0530

    Implemented how to Dynamically create Array and 2D array in Heap Memory

---
+++ Dynamic Memory Allocation/ArrayMemoryAllocationINHeapMemory.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int * arr = new int [4];
+
+    for(int i=0; i<4; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<4; i++){
+        cout<<arr[i];
+    }
+}
